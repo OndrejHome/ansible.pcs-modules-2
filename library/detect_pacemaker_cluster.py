4cdc473872f70d154569cdaddc646023804c3284
Ondrej Famera <ondrej@famera.cz>
2018-12-12 21:43:06 +0100

    various fixes for fact detection module
    - removed 'options:' from documentation to prevent sanity tests crashing
    - added missing metadata, author details
    - removed some PEP8 related issues

--- library/detect_pacemaker_cluster.py
+++ library/detect_pacemaker_cluster.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python
+# Copyright: (c) 2018, Ondrej Famera <ondrej-xa2iel8u@famera.cz>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Apache License v2.0 (see LICENSE-APACHE2.txt or http://www.apache.org/licenses/LICENSE-2.0)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
 
 DOCUMENTATION = '''
 ---
 module: detect_pacemaker_cluster
+author: "Ondrej Famera (@ondrejHome)"
 short_description: detect facts about installed pacemaker cluster
 description:
      - Module for collecting various information about pacemaker cluster
 version_added: "2.4"
-options:
 notes:
    - Tested on CentOS 7.5
    - works only with pacemaker clusters that uses /etc/corosync/corosync.conf
 requirements: [ ]
-author: "Ondrej Famera <ondrej-xa2iel8u@famera.cz>"
 '''
 
 EXAMPLES = '''
@@ -20,18 +31,21 @@ EXAMPLES = '''
 
 '''
 
+import re
+from ansible.module_utils.basic import AnsibleModule
 
-def main():
+
+def run_module():
         module = AnsibleModule(
-                argument_spec=dict(),
-                supports_check_mode=True
+            argument_spec=dict(),
+            supports_check_mode=True
         )
 
         result = {}
 
         try:
             corosync_conf = open('/etc/corosync/corosync.conf', 'r')
-            nodes = re.compile(r"node\s*\{([^}]+)\}", re.M+re.S)
+            nodes = re.compile(r"node\s*\{([^}]+)\}", re.M + re.S)
             nodes_list = nodes.findall(corosync_conf.read())
             node_list_set = set()
             if len(nodes_list) > 0:
@@ -49,11 +63,12 @@ def main():
         except IOError as e:
             result['ansible_facts'] = {}
             result['ansible_facts']['pacemaker_cluster_present'] = False
-        except OSError as e:
-            result['ansible_facts'] = {}
-            result['ansible_facts']['pacemaker_cluster_present'] = False
         module.exit_json(**result)
 
-# import module snippets
-from ansible.module_utils.basic import *
-main()
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()

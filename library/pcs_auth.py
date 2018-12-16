474131e82988ce078599ef0e44f2697162514351
Ondrej Famera <ondrej@famera.cz>
2018-12-16 14:00:04 +0100

    experimental support for pcs 0.10 without attribute changes
    relevant for RHEL 8.0 Beta, Fedora 29

--- library/pcs_auth.py
+++ library/pcs_auth.py
@@ -46,6 +46,7 @@ notes:
     node 2 doesn't mean that node 2 is authenticated agains node 1!
   - Tested on CentOS 6.8, 7.3
   - Tested on Red Hat Enterprise Linux 7.3, 7.4, 7.6
+  - Experimental support for Red Hat Enterprise Linux 8.0 Beta and pcs 0.10
 '''
 
 EXAMPLES = '''
@@ -97,11 +98,23 @@ def run_module():
         if find_executable('pcs') is None:
             module.fail_json(msg="'pcs' executable not found. Install 'pcs'.")
 
-        if os.path.isfile('/var/lib/pcsd/tokens'):
+        # get the pcs major.minor version
+        rc, out, err = module.run_command('pcs --version')
+        if rc == 0:
+            pcs_version = out.split('.')[0] + '.' + out.split('.')[1]
+        else:
+            module.fail_json(msg="pcs --version exited with non-zero exit code (" + rc + "): " + out + error)
+
+        if os.path.isfile('/var/lib/pcsd/tokens') and pcs_version == '0.9':
             tokens_file = open('/var/lib/pcsd/tokens', 'r+')
             # load JSON tokens
             tokens_data = json.load(tokens_file)
             result['tokens_data'] = tokens_data['tokens']
+        if os.path.isfile('/var/lib/pcsd/known-hosts') and pcs_version == '0.10':
+            tokens_file = open('/var/lib/pcsd/known-hosts', 'r+')
+            # load JSON tokens
+            tokens_data = json.load(tokens_file)
+            result['tokens_data'] = tokens_data['known_hosts']
 
         rc, out, err = module.run_command('pcs cluster pcsd-status %(node_name)s' % module.params)
 
@@ -109,23 +122,42 @@ def run_module():
             # WARNING: this will also consider nodes to which we cannot connect as unauthorized
             result['changed'] = True
             if not module.check_mode:
-                cmd_auth = 'pcs cluster auth %(node_name)s -u %(username)s -p %(password)s --local' % module.params
+                if pcs_version == '0.9':
+                    cmd_auth = 'pcs cluster auth %(node_name)s -u %(username)s -p %(password)s --local' % module.params
+                elif pcs_version == '0.10':
+                    cmd_auth = 'pcs host auth %(node_name)s -u %(username)s -p %(password)s' % module.params
+                else:
+                    module.fail_json(msg="unsupported version of pcs (" + pcs_version + "). Only versions 0.9 and 0.10 are supported.")
                 rc, out, err = module.run_command(cmd_auth)
                 if rc == 0:
                     module.exit_json(**result)
                 else:
                     module.fail_json(msg="Failed to authenticate node using command '" + cmd_auth + "'", output=out, error=err)
 
-        elif state == 'absent' and tokens_data and node_name in tokens_data['tokens']:
+        elif (state == 'absent' and tokens_data and (
+                (pcs_version == '0.9' and node_name in tokens_data['tokens']) or
+                (pcs_version == '0.10' and node_name in tokens_data['known_hosts'])
+                )):
             result['changed'] = True
             if not module.check_mode:
-                del tokens_data['tokens'][node_name]
-                del tokens_data['ports'][node_name]
-                tokens_data['data_version'] += 1
-                # write the change into token file
-                tokens_file.seek(0)
-                json.dump(tokens_data, tokens_file, indent=4)
-                tokens_file.truncate()
+                if pcs_version == '0.9':
+                    del tokens_data['tokens'][node_name]
+                    del tokens_data['ports'][node_name]
+                    tokens_data['data_version'] += 1
+                    # write the change into token file
+                    tokens_file.seek(0)
+                    json.dump(tokens_data, tokens_file, indent=4)
+                    tokens_file.truncate()
+                elif pcs_version == '0.10':
+                    cmd_deauth = 'pcs host deauth %(node_name)s' % module.params
+                    rc, out, err = module.run_command(cmd_deauth)
+                    if rc == 0:
+                        module.exit_json(**result)
+                    else:
+                        module.fail_json(msg="Failed to de-authenticate node using command '" + cmd_deauth + "'", output=out, error=err)
+                else:
+                    module.fail_json(msg="unsupported version of pcs (" + pcs_version + "). Only versions 0.9 and 0.10 are supported.")
+
         else:
             result['changed'] = False
             module.exit_json(**result)
